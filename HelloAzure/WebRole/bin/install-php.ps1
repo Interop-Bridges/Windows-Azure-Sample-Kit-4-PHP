57400435dc3acb94720f6c361ebdde50c34129c4
blobaugh <ben@lobaugh.net>
2011-10-04 17:27:03 -0500

    Updated the samples to remove the scaffold. This is necessary as the scaffold is updating almost weekly and quickly outdating these samples. Users now have the choice of how to put the samples into scaffolds, or they can use the paz tool to automate the scaffolding and packaging at http://github.com/blobaugh/WAZ-PHP-Utils

--- HelloAzure/WebRole/bin/install-php.ps1
+++
@@ -1,5 +0,0 @@
-[Reflection.Assembly]::LoadWithPartialName("Microsoft.WindowsAzure.ServiceRuntime")
-
-if ([Microsoft.WindowsAzure.ServiceRuntime.RoleEnvironment]::CurrentRoleInstance.Id.Contains('_IN_')) {
-    .\install-php-impl.cmd
-}
